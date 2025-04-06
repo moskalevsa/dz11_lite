d20af6891ef34f9aaa792f42305e13b8aa261f0e
moskalevsa <moskalevsa@yandex.ru>
2025-04-06 11:26:31 +0300

    содержится функция generate_roport

---
+++ library_manager/report.py
@@ -0,0 +1,14 @@
+
+"""
+Модуль `report.py
+"""
+from library_manager import Library
+
+def generate_report(library: Library):
+    """
+    Генерирует отчет о всех книгах в библиотеке в формате строки.
+    :param library - библиотека
+    """
+    library.viewing_books()
+    return
+
